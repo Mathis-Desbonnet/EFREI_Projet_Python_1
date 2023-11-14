766e60d78001ab1665aa3f6b05acd64f84711a15
Arnaud <chadal.arnaud2@gmail.com>
2023-11-14 11:30:36 +0100

    add menu.py

--- menu.py
+++ menu.py
@@ -1,28 +1,16 @@
-from tkinter import *
-from tkinter import messagebox
-from tkinter.simpledialog import askstring
-root = Tk()
-root.configure(background="#2D2B30")
-root.minsize(200, 200)
-root.geometry("300x300+500+500")
+import PySimpleGUI as sg
+sg.theme('DarkAmber')   # Add a little color to your windows
+# All the stuff inside your window. This is the PSG magic code compactor...
+layout = [  [sg.Text('Some text on Row 1')],
+            [sg.Text('Enter something on Row 2'), sg.InputText()],
+            [sg.OK(), sg.Cancel()]]
 
+# Create the Window
+window = sg.Window('Window Title', layout)
+# Event Loop to process "events"
+while True:             
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Cancel'):
+        break
 
-label = Label(root, 
-                text = "Label_1",
-                bg = "#2D2B30",
-                fg = "white",
-                font = ("Impact", 60))
-label.pack()
-#def test() :
-#    msg=messagebox.showinfo( "Hello Python", "Hello World")
-#B = Button(root, text ="Hello", command = test())
-#B.place(x=50,y=50)
-
-def show():
-   name = askstring("Input", "Enter you name")
-   print(name)
-def pr() :
-   print("test")
-B = Button(root, text ="Click", command = pr)
-B.place(x=50,y=50)
-root.mainloop()
+window.close()
