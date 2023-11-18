82bb465c4eed2a7deb2c1ab32c837fa3a881a2dc
Arnaud <chadal.arnaud2@gmail.com>
2023-11-18 21:08:56 +0100

    Display menu

--- menu.py
+++ menu.py
@@ -1,18 +1,22 @@
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')   # Add a touch of color
+sg.theme('DarkBlue12')   # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+layout = [  [sg.Combo(['irrelevantWords', 'importantWords', 'mostUsedWords', 'whoTalkAbout', 'firstToSay', 'universalWords']), sg.Text("Choose the function")],
+            [sg.InputText(size=(20, 1)), sg.Text("Enter the arguments")],
+            [sg.Push(), sg.Output(size=(40, 20))],
+            [sg.Button('Launch'), sg.Push(), sg.Button('Close Window')]
+        ]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Test', layout).Finalize()
+#window.Maximize()
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event in (None, 'Close Window'): # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+    print('You entered in the textbox:')
+    print(values['textbox'])  # get the content of multiline via its unique key
 
 window.close()
