f797931b1998e4611ff5b28816eb31999aa53934
Arnaud <chadal.arnaud2@gmail.com>
2023-11-20 20:26:31 +0100

    Fix whoTalkAbout and listOfWords functions

--- main.py
+++ main.py
@@ -1,17 +1,74 @@
 import PySimpleGUI as sg
+import os
+import TF_IDF_functions as tfidf
+import other_functions as of
+
 
 sg.theme('DarkBlue12')
-layout = [  [sg.Combo(['irrelevantWords', 'importantWords', 'mostUsedWords', 'whoTalkAbout', 'firstToSay', 'universalWords']), sg.Text("Choose the function")],
-            [sg.InputText(size=(20, 1)), sg.Text("Enter the arguments")],
-            [sg.Push(), sg.Output(size=(40, 20))],
-            [sg.Button('Launch'), sg.Push(), sg.Button('Close Window')]
+comboList = ['irrelevantWords', 'importantWords', 'mostUsedWords', 'whoTalkAbout', 'firstToSay', 'universalWords']
+layout1 = [  [sg.InputText(size=(20, 1), k="AddrInput"), sg.Text("Enter the directory path")],
+            [sg.Combo(comboList, k="FunctionInput"), sg.Text("Choose the function")],
+            [sg.Button('Next Step')],
         ]
+window1 = sg.Window('main', layout1).Finalize()
+run = True
 
-window = sg.Window('Test', layout).Finalize()
+while run:
+    event, values = window1.read()
+    
+    if event in ('Close Window'):
+        run = False
 
-while True:
-    event, values = window.read()
-    if event in (None, 'Close Window'):
-        break
+    if event in ('Next Step') and values["FunctionInput"] != "" and values["AddrInput"] != "":
 
-window.close()
+        path = values["AddrInput"]
+        function = values["FunctionInput"]
+
+        if os.path.exists(path) == False:
+            layout3 = [  [sg.Text("Path doesn't exist")]
+                    ]
+            window3 = sg.Window('ErrorPath', layout3, keep_on_top=True).Finalize()
+
+            run = False
+            run2 = False
+            run3 = True
+            window1.close()
+        else:
+            if values["FunctionInput"] == "whoTalkAbout" or values["FunctionInput"] == "firstToSay":
+                layout2 = [[sg.InputText(size=(20, 1)), sg.Text("Enter the arguments")], 
+                        [sg.Multiline(size=(40, 20), disabled=True, key="Output")],
+                        [sg.Button('Go')]]
+            else:
+                layout2 = [[sg.Push(), sg.Multiline(size=(40, 20), disabled=True, key="Output")],
+                        [sg.Button('Go')]]
+            window2 = sg.Window('next', layout2).Finalize()
+            Output = window2.FindElement("Output")
+            tfidfScore = tfidf.TFIDFList(path)[0]
+            tfidfWords = tfidf.TFIDFList(path)[1]
+
+            run = False
+            run2 = True
+            run3 = False
+            window1.close()
+
+while run2:
+    event, values = window2.read()
+    if event in ('Close Window'):
+        run2 = False
+        window2.close()
+    if event == "Go":
+        Output.Update(disabled=False)
+        match function: 
+            case "irrelevantWords": Output.Update(of.irrelevantWords(tfidfScore, tfidfWords))
+            case "importantWords": Output.Update(of.importantWords(tfidfScore, tfidfWords))
+            #case "mostUsedWords": Output.Update(of.mostUsedWords(argument, path))
+            #case "whoTalkAbout": Output.Update(of.whoTalkAbout(argument, path))
+            #case "firstToSay": Output.Update(of.firstToSay(argument))
+            case "irrelevantWords": Output.Update(of.irrelevantWords(tfidfScore, tfidfWords))
+        Output.Update(disabled=True)
+
+while run3:
+    event, values = window3.read()
+    if event in ('Close Window'):
+        run3 = False
+        window3.close()
--- other_functions.py
+++ other_functions.py
@@ -1,141 +1,166 @@
-from TF_IDF_functions import TFCalculator, TFIDFList
+from TF_IDF_functions import TFIDFList, occurrenceOfWords
 import os
 from fonctions import getPresidentNames
 
-def irrelevantWords(matrice : list, wordsList : list) :
-    '''
+
+def irrelevantWords(matrice: list, wordsList: list):
+    """
     Return a string wich contains the irrelevants words (TD-IDF = 0)
-    '''
+    """
     irrelevants = ""
-    for i in range(len(matrice)) :
+    for i in range(len(matrice)):
         word = matrice[i]
         isImportant = True
         nbr = 0
-        while (nbr < len(word)) and isImportant :
-            if word[nbr] != 0 :
+        while (nbr < len(word)) and isImportant:
+            if word[nbr] != 0:
                 isImportant = False
             nbr += 1
-        irrelevants += (wordsList[i] + "\n")*isImportant
+        irrelevants += (wordsList[i] + "\n") * isImportant
     irrelevants = irrelevants[:-1]
     return irrelevants
 
-#print(irrelevantWords(TFIDFList()[0], TFIDFList()[1]))
 
-def importantWords(matrice : list, wordsList : list) :
-    '''
+# print(irrelevantWords(TFIDFList("./test/")[0], TFIDFList("./test/")[1]))
+# print()
+
+
+def importantWords(matrice: list, wordsList: list):
+    """
     Return a string wich contains the importants words (high TD-IDF)
-    '''
+    """
     maxTFIDF = 0
     wordsAndTFIDFScore = {}
     betterWords = ""
-    for i in range(len(matrice)) :
+    for i in range(len(matrice)):
         maxTFIDFperWords = 0
-        for j in range(len(matrice[i])) :
-            if matrice[i][j] != None and matrice[i][j] > maxTFIDFperWords :
+        for j in range(len(matrice[i])):
+            if matrice[i][j] != None and matrice[i][j] > maxTFIDFperWords:
                 maxTFIDFperWords += matrice[i][j]
         wordsAndTFIDFScore[wordsList[i]] = maxTFIDFperWords
-        if maxTFIDF < maxTFIDFperWords :
+        if maxTFIDF < maxTFIDFperWords:
             maxTFIDF = maxTFIDFperWords
-    for key in wordsAndTFIDFScore.keys() :
-        if wordsAndTFIDFScore[key] == maxTFIDF :
+    for key in wordsAndTFIDFScore.keys():
+        if wordsAndTFIDFScore[key] == maxTFIDF:
             betterWords += key + "\n"
     betterWords = betterWords[:-1]
     return betterWords
 
-#print(importantWords(TFIDFList()[0], TFIDFList()[1]))
 
-def listOfWords(president : str, folderAddr : str="./cleaned/") :
+# print(TFIDFList("./test/")[0], TFIDFList("./test/")[1])
+# print(importantWords(TFIDFList("./test/")[0], TFIDFList("./test/")[1]))
+
+
+def listOfWords(president : str, irrevelantsWordsList : list=None, folderAddr : str="./cleaned/") :
     '''
     Return a dictionnary with the words used by the chosen president (except the irrelevants words)
     '''
-    relevantUsedWords = {}
-    irrelevants = irrelevantWords(TFIDFList()[0], TFIDFList()[1])
+    relevantUsedWords = []
+    irrelevants = irrevelantsWordsList
     for fileName in os.listdir(folderAddr):
         if president in fileName :
-            allUsedWords = TFCalculator(open(folderAddr + fileName, "r").read())
-            for keys in allUsedWords.keys() :
+            text = open(folderAddr + fileName, "r").read()
+            allUsedWords = list(set(text.split()))
+            for keys in allUsedWords :
                 if keys not in irrelevants :
-                    if keys in relevantUsedWords.keys() :
-                        relevantUsedWords[keys] += allUsedWords[keys]
-                    else :
-                        relevantUsedWords[keys] = allUsedWords[keys]
-    relevantUsedWords = dict(sorted(relevantUsedWords.items(), key = lambda x : x[1], reverse = True))
+                        relevantUsedWords.append(keys)
     return relevantUsedWords
 
-#print(listOfWords("Macron"))
+#print(listOfWords("Macron", irrelevantWords(TFIDFList("./cleaned/")[0], TFIDFList("./cleaned/")[1])))
 
-def mostUsedWords(president : str, folderAddr : str="./cleaned/") :
-    '''
+
+
+def mostUsedWords(president: str, folderAddr: str = "./cleaned/"):
+    """
     Return a string wich contains the most used words by the chosen president
-    '''
+    """
     presidentWordsList = listOfWords(president, folderAddr)
     mostUsedWords = ""
-    for keys in list(presidentWordsList.keys())[:10] :
+    for keys in list(presidentWordsList)[:10]:
         mostUsedWords += keys + "\n"
     mostUsedWords = mostUsedWords[:-1]
     return mostUsedWords
 
 
- #print(mostUsedWords("Macron"))
+#print(mostUsedWords("Macron"))
 
 
-def whoTalkAbout(word : str, folderAddr : str="./cleaned/") :
-    '''
+def whoTalkAbout(word: str, folderAddr: str = "./cleaned/"):
+    """
     Return the names of the presidents who has talked about a chosen word and the name of the president who has talked the most about it
-    '''
+    """
     namesList = getPresidentNames()
     presidentsList = []
-    for name in namesList :
-        if not name in presidentsList :
+    irrevelant = irrelevantWords(TFIDFList(folderAddr)[0], TFIDFList(folderAddr)[1])
+    for name in namesList:
+        if not name in presidentsList:
             presidentsList.append(name)
     hasTalkAbout = ""
     maxi = ["", 0]
-    for president in presidentsList :
-        presidentWordsList = listOfWords(president)
-        if word in presidentWordsList.keys() :
+    for president in presidentsList:
+        presidentWordsList = listOfWords(president, irrevelant, folderAddr)
+        if word in presidentWordsList:
             hasTalkAbout += president + "\n"
-            if maxi[1] < presidentWordsList[word] :
-                maxi = [president, presidentWordsList[word]]
+            occurences = 0
+            for fileName in os.listdir(folderAddr):
+                if president in fileName :
+                    occurences += occurrenceOfWords(open(folderAddr + fileName, "r").read(), word)
+            if maxi[1] < occurences:
+                maxi = [president, occurences]
     hasTalkAbout += "\n" + "The president who has talked the most about is : " + maxi[0]
     return hasTalkAbout
-        
+
+
 #print(whoTalkAbout("nation"))
 
 
-def firstToSay(words : list) :
-    '''
+def firstToSay(words: list):
+    """
     Return the name of the president who has talked about the words the first
-    '''
-    chronology = ["De Gaulle", "Pompidou", "Giscard", "Mitterrand", "Chirac", "Sarkozy", "Hollande", "Macron"]
-    for president in chronology :
+    """
+    chronology = [
+        "De Gaulle",
+        "Pompidou",
+        "Giscard",
+        "Mitterrand",
+        "Chirac",
+        "Sarkozy",
+        "Hollande",
+        "Macron",
+    ]
+    for president in chronology:
         presidentWordsList = listOfWords(president)
-        for word in words :
-            if word in presidentWordsList.keys() :
+        for word in words:
+            if word in presidentWordsList:
                 return president
-            
-#print(firstToSay(["climat"]))
 
 
-def universalWords(matrice : list, wordsList : list) :
-    '''
+# print(firstToSay(["climat"]))
+
+
+def universalWords(wordsList: list, irrelevants : list, folderAddr: str = "./cleaned/"):
+    """
     Return a string wich contains the words used by all the presidents (except the irrelevants words)
-    '''
-    irrelevants = irrelevantWords(matrice, wordsList)
-    text = ""
-    for i in range(len(matrice)) :
-        val = True
-        counter = 0
-        if wordsList[i] in irrelevants :
-            val = False
-        while counter < len(matrice[i]) and val :
-            if matrice[i][counter] == None :
-                val = False
-            counter += 1
-        text += (wordsList[i] + "\n")*val
-    if text == "" :
-        return "No words has been used by all the presidents apart from the irrelevants words"
-    else :
-        text =  text[:-1]
-        return text
+    """
+    universals = ""
+    namesList = getPresidentNames()
+    presidentWordsList = []
+    print("Please wait, it can take a few seconds...")
+    for president in namesList:
+        presidentWordsList.append(listOfWords(president, irrelevants, folderAddr))
+    for word in wordsList:
+        if not word in irrelevants:
+            val = True
+            counter = 0
+            while val and counter < len(presidentWordsList) :
+                president = presidentWordsList[counter]
+                if not word in president :
+                    val = False
+                counter += 1
+            universals += (word + "\n")*val
+    if universals == "" :
+        return "No important word has been used by all the presidents."
+    else : return universals
 
-#print(universalWords(TFIDFList()[0], TFIDFList()[1]))
+# print()
+# print(universalWords(TFIDFList()[1] , irrelevants))
