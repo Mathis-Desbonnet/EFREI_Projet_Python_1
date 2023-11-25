ac50f13d3358e467c69ca6fb337b4ab5becb66fa
Arnaud <chadal.arnaud2@gmail.com>
2023-11-25 15:48:45 +0100

    Add comments in main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import PySimpleGUI as sg
+import PySimpleGUI as sg # Import the library wich will be used to create the GUI.
 import os
 import TF_IDF_functions as tfidf
 import other_functions as of
 
 
 sg.theme("DarkBlue12")
-comboList = [
+comboList = [ # Create a list of all the functions the user can use.
     "irrelevantWords",
     "importantWords",
     "mostUsedWords",
@@ -15,26 +15,23 @@ comboList = [
 ]
 path = ""
 
-layout1 = [
-    [
-        sg.InputText(size=(20, 1), default_text=path, k="AddrInput"),
-        sg.Text("Enter the directory path"),
-    ],
-    [sg.Combo(comboList, k="FunctionInput"), sg.Text("Choose the function")],
-    [sg.Button("Next Step")],
+layout1 = [ # Create the layout of the first window.
+    [sg.InputText(size=(20, 1), default_text=path, k="AddrInput"), sg.Text("Enter the directory path")], # Create the input where the user will enter the path and the text next to the input box.
+    [sg.Combo(comboList, k="FunctionInput"), sg.Text("Choose the function")], # Create the drop down list where the user will choose the function to use.
+    [sg.Button("Next Step")], #Create the button to go to the second window.
 ]
 window1 = sg.Window("main", layout1).Finalize()
 
 
-def firstWindow(run, window):
+def firstWindow(run, window): # Create the first window (the one where the user will enter the path and the function he wish to use).
     print("open first  window")
-    while run:
-        event, values = window.read()
+    while run: # The loop wich will keep the window open until the user close it or click on the "Next Step" button.
+        event, values = window.read() # Get the event and the values of the window.
 
-        if event in ("Close Window"):
+        if event in ("Close Window"): # Check if the user closed the window.
             run = False
 
-        if (
+        if ( # Check if the user has entered a path and a function before clicking on the "Next Step" button.
             event in ("Next Step")
             and values["FunctionInput"] != ""
             and values["AddrInput"] != ""
@@ -42,33 +39,23 @@ def firstWindow(run, window):
             path = values["AddrInput"]
             function = values["FunctionInput"]
 
-            if os.path.exists(path) == False:
-                layout3 = [[sg.Text("Path doesn't exist")]]
+            if os.path.exists(path) == False: # Checl if the path entered by the user exist.
+                layout3 = [[sg.Text("Path doesn't exist")]] # If not, create an error window.
                 window3 = sg.Window("ErrorPath", layout3, keep_on_top=True).Finalize()
 
                 run = False
                 window.close()
                 ErrorWindow(True, window3)
             else:
-                if values["FunctionInput"] in [
-                    "whoTalkAbout",
-                    "firstToSay",
-                    "mostUsedWords",
-                ]:
-                    layout2 = [
-                        [
-                            sg.InputText(size=(20, 1), k="ArgumentInput"),
-                            sg.Text("Enter the arguments"),
-                        ],
+                if values["FunctionInput"] in ["whoTalkAbout", "firstToSay", "mostUsedWords"]:
+                    layout2 = [ # Create the second window with an input box for the argument if the function the user wish to use require one.
+                        [sg.InputText(size=(20, 1), k="ArgumentInput"), sg.Text("Enter the arguments")],
                         [sg.Multiline(size=(40, 20), disabled=True, key="Output")],
                         [sg.Button("Go"), sg.Push(), sg.Button("Previous")],
                     ]
                 else:
-                    layout2 = [
-                        [
-                            sg.Push(),
-                            sg.Multiline(size=(40, 20), disabled=True, key="Output"),
-                        ],
+                    layout2 = [ # Create the second window without an input box for the argument if the function the user wish to use doesn't require one.
+                        [sg.Push(), sg.Multiline(size=(40, 20), disabled=True, key="Output")],
                         [sg.Button("Go"), sg.Push(), sg.Button("Previous")],
                     ]
 
@@ -77,7 +64,7 @@ def firstWindow(run, window):
                 LoadingWindow(True, layout2, path, function)
 
 
-def LoadingWindow(run, layout2, path, function):
+def LoadingWindow(run, layout2, path, function): # Create a loading window (the one that will be displayed while the program is creating all the needed variables).
     layout4 = [[sg.Text("Chargement.....")]]
     window4 = sg.Window("LoadingWindow", layout4, keep_on_top=True).Finalize()
     event, values = window4.read(100)
@@ -89,22 +76,20 @@ def LoadingWindow(run, layout2, path, function):
         Output = window2.find_element("Output")
         run = False
         window4.close()
-        secondWindow(
+        secondWindow( # Launch the second window and create all the variables needed.
             True,
             window2,
             layout2,
             Output,
-            path,
-            function,
-            tfidfScore,
-            tfidfWords,
-            irrelevants,
+            path, # The path to the directory, entered by the user in the first window.
+            function, # The function the user want to use, entered by the user in the first window.
+            tfidfScore, # The list of the TFIDF scores, create by using TFIDFList function.
+            tfidfWords, # The list of the words, create by using TFIDFList function.
+            irrelevants, # The list of the irrelevants words, create by using irrelevantWords function.
         )
 
 
-def secondWindow(
-    run, window, layout, Output, path, functions, tfidfScore, tfidfWords, irrelevants
-):
+def secondWindow(run, window, layout, Output, path, functions, tfidfScore, tfidfWords, irrelevants):
     while run:
         event, values = window.read()
         if len(layout) == 3:
@@ -113,8 +98,8 @@ def secondWindow(
             run = False
             window.close()
         if event == "Go":
-            Output.Update(disabled=False)
-            match functions:
+            Output.Update(disabled=False) # Allow to write in the output box.
+            match functions: # Check wich function have to be used and use the functions of the other_functions.py file.
                 case "irrelevantWords":
                     output = irrelevants
                 case "importantWords":
@@ -129,31 +114,25 @@ def secondWindow(
                 case "universalWords":
                     output = of.universalWords(tfidfWords, irrelevants, path)
             Output.Update("")
-            if type(output) == list:
+            if type(output) == list: # Iterate tough the output if it's a list to display it in the output box.
                 for i in output:
                     Output.Update(i + "\n", append=True)
             else:
-                Output.Update(output)
+                Output.Update(output) # Display the output in the output box if it's just a string.
             Output.Update(disabled=True)
-        if event in ("Previous"):
+        if event in ("Previous"): # Go back to the first window if the user click on the "Previous" button.
             run = False
             window.close()
             layout1 = [
-                [
-                    sg.InputText(size=(20, 1), default_text=path, k="AddrInput"),
-                    sg.Text("Enter the directory path"),
-                ],
-                [
-                    sg.Combo(comboList, k="FunctionInput"),
-                    sg.Text("Choose the function"),
-                ],
-                [sg.Button("Next Step")],
+                [sg.InputText(size=(20, 1), default_text=path, k="AddrInput"), sg.Text("Enter the directory path")],
+                [sg.Combo(comboList, k="FunctionInput"), sg.Text("Choose the function")],
+                [sg.Button("Next Step")]
             ]
             window1 = sg.Window("main", layout1).Finalize()
-            firstWindow(True, window1)
+            firstWindow(True, window1) # Reopen the first window.
 
 
-def ErrorWindow(run, window):
+def ErrorWindow(run, window): # Create the error window (the one that will be displayed if the user enter a wrong path)
     while run:
         event, values = window.read()
         if event in ("Close Window"):
@@ -162,4 +141,4 @@ def ErrorWindow(run, window):
 
 run = True
 
-firstWindow(run, window1)
+firstWindow(run, window1) # The program starts here
